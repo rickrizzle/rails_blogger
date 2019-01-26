3cc793527f6e4bb357f7a3a74aa1bb233d742cc9
rickrizzle <rickh39@gmail.com>
2019-01-26 17:43:51 -0500

    Fixed form rendering issue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,9 @@ class ArticlesController < ApplicationController
     def destroy 
         @article = Article.find(params[:id])
         @article.destroy
+
+        flash.notice = "Article '#{@article.title}' Deleted!"
+
         redirect_to articles_path(@articles)
     end
     def edit
@@ -28,6 +31,8 @@ class ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id])
         @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
 
         redirect_to article_path(@article)
     end
